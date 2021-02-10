b2e7e5b1461a2dada860adbbc60e213e50ee3455
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-02-09 18:18:01 -0600

    added test package and created getters/setters for CoreCompetency class

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,12 @@
+package org.launchcode.techjobs_oo.Tests;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class JobTest {
+//    @Test
+//    public void testDvdName(){
+//        //DVD avengers = new DVD("Avengers", 50, "Action", "Blue-Ray");
+//        assertEquals(avengers.getName(), "Avengers");
+//    }
+}
