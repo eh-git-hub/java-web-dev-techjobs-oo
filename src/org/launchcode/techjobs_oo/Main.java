79f5cd35d87c1ac48527ae19060098d8fa1c8e66
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-02-11 21:10:25 -0600

    created JobField, extended all classes, and commented repeated code

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-//        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-//        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
 
 //        Job jobTest1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
 //        Job jobTest2 = new Job("Video Producer", new Employer("Donut Media"), new Location("Los Angeles, CA"), new PositionType("Video Producer"), new CoreCompetency("Make car videos"));
@@ -17,14 +17,14 @@ public class Main {
 //        Job jobTest6 = new Job("Construction Worker", new Employer("CAT"), new Location("Fayetteville, AR"), new PositionType(""), new CoreCompetency("Build"));
 //        Job jobTest7 = new Job("Race Car Driver", new Employer("F1"), new Location("Turin, Italy"), new PositionType("Main Driver"), new CoreCompetency(""));
 
-//        ArrayList<Job> jobs = new ArrayList<>();
-//        jobs.add(job1);
-//        jobs.add(job2);
-//        jobs.add(job3);
-//
-//        for (Job job : jobs){
-//            System.out.println(job);
-//        }
+        ArrayList<Job> jobs = new ArrayList<>();
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+
+        for (Job job : jobs){
+            System.out.println(job);
+        }
 
 //        System.out.println(jobTest1.toString());
 //        System.out.println(jobTest2.toString());
